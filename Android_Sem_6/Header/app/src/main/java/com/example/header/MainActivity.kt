1d0aa56ca1baed915561f07f4cd42990a1b0d116
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2024-01-22 22:00:18 +0530

    22-01-24 22:00

---
+++ Android_Sem_6/Header/app/src/main/java/com/example/header/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.header
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mymenu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
