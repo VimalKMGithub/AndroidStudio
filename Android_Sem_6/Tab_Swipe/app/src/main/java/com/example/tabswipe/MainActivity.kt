319076586f89ea52298c315ba28247aca3771881
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2024-04-08 23:15:30 +0530

    .

---
+++ Android_Sem_6/Tab_Swipe/app/src/main/java/com/example/tabswipe/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.tabswipe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val fragments = listOf(FirstFragment(), SecondFragment())
+        val adapter = ViewPagerAdapter(fragments, supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
+    }
+}
